2fd682f20a34a933128a98e88981ed47604c38cb
Jared Berghold <jared@intersect.org.au>
2015-12-22 11:55:12 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,121 @@
+# owntime-manager
+Python helper script for managing virtual machines (VMs) on Intersect's OwnTime (<http://www.intersect.org.au/content/owntime-cloud-computing>).
+
+The script assists with the creation and management of large numbers of VMs on OwnTime. The typical scenario in which it would be used is a university IT department that needs to create and manage a number of VMs for each of their users.
+
+The script has been designed to work with Intersect's OwnTime cloud computing infrastructure. However, with some minor tweaking the script can also be used with the Nectar Cloud or indeed any cloud computing infrastructure based on OpenStack. 
+
+The script uses the OpenStack Python SDK: <http://developer.openstack.org/sdks/python/openstacksdk/users/index.html>
+
+The script has been developed against Python 2.7.10.
+
+## Installation
+
+### Install OpenStack Python SDK
+Install the OpenStack Python SDK using `pip` (`pip` installation instructions: <https://pip.pypa.io/en/stable/installing>):
+
+    pip install openstacksdk
+
+### Setup clouds.yaml configuration file
+The OpenStack Python SDK uses a clouds.yaml file to store configration information. 
+
+The `clouds.yaml` file used by `owntime-manager` needs to contain authentication details for a cloud defined with the name `nectar`, as follows:
+
+    clouds:
+      nectar:
+        auth:
+          auth_url: https://keystone.rc.nectar.org.au:5000/v2.0/
+          username: <Replace with your OpenStack username>
+          password: <Replace with your OpenStack API password>
+          project_name: <Replace with your OpenStack project name>
+
+Rename the `clouds.yaml.example` file to `clouds.yaml` and update it with your Nectar Cloud username (i.e. your AAF email address), Nectar Cloud API password and Nectar Cloud project name.
+
+If you do not know your Nectar Cloud API password, follow these steps:
+
+1. Go to <https://dashboard.rc.nectar.org.au>. If you are directed to the AAF login page, log in with your AAF credentials.
+1. Click the button labelled with your AAF email address (i.e your Nectar Cloud username) in the top right menu, then click the 'Settings' link.
+1. On the left-hand side menu, click 'Reset Password'.
+1. Click the 'Reset Password' button to generate a new password.
+1. Copy the generated password to the `password` field in the `clouds.yaml` file. 
+          
+The OpenStack Python SDK will look for `clouds.yaml` in a few locations. Copy the `clouds.yaml` file to one of these locations:
+
+* Current Directory
+* `~/.config/openstack`
+* `/etc/openstack`
+
+Alternatively, to use `clouds.yaml` in a user-defined location, set the environment variable `OS_CLIENT_CONFIG_FILE` to the absolute path of the file. For example:
+
+    export OS_CLIENT_CONFIG_FILE=/path/to/my/config/clouds.yaml
+
+### Create a symbolic link to owntime-manager.py
+To make it more convenient to run `owntime-manager.py`, make a symbolic link to it. For example:
+
+    ln -s /path/to/owntime-manager.py /usr/local/bin/owntime
+
+## Usage
+Usage instructions are as follows:
+
+    Usage:
+    
+       Create a new VM: 	
+          owntime vm create <user_id> [-f "flavour name" -i "image name"]
+    
+       List VMs for given user:
+          owntime user <user_id>
+    
+       Manage VM for given user:
+          owntime vm [status|suspend|resume|reboot|delete] <vm_name>
+
+Note: These usage instructions assume a symbolic link has been created to `owntime-manager.py` as shown above. 
+
+### Examples
+
+#### Create a new VM
+To create a new VM, use  `vm create`. For example, to create a new VM for a user called FN-2187, run the following command: 
+
+    owntime vm create FN-2187
+    
+This will create a VM called 'FN-2187-0' using the default flavour 'm2.medium' and default image 'NeCTAR CentOS 7 x86_64'. The appended number (i.e. '-0') is added automatically by the script. This makes it easier to track multiple VMs belonging to a single user. 
+
+If a keypair does not already exist for user FN-2187, the script will automatically generate one. The keypair can then be provided to the user so that he or she may access the VM using SSH. 
+
+To create another VM for the same user, run the same command again. This will create a second VM, this time called 'FN-2187-1'.
+
+A user-defined flavour and image can be chosen by using the `-f` and `-i` flags, respectively. For example:
+
+    owntime vm create FN-2187 -f "m2.xlarge" -i "NeCTAR OpenSuSE 13.2 x86_64"
+
+A list of Nectar Cloud public images can be found using the following link: <https://wiki.rc.nectar.org.au/wiki/Image_Catalog>. Note that this list is not kept up-to-date.
+
+For an up-to-date list of images, log into <https://dashboard.rc.nectar.org.au> and click the 'Images' link on the left-hand side. 
+    
+A list of availale flavours can be accessed via the Nectar Cloud dashboard. Log into <https://dashboard.rc.nectar.org.au> and click the 'Instances' link on the left-hand side. Then click on the 'Launch Instance' button at the top right. Available flavours can be seen in the 'Flavor' drop-down. 
+
+#### List VMs
+To list the VMs for a given user, use the `user` command. For example:
+
+    owntime user FN-2187
+    
+#### Manage a VM
+To get the **status** for a particular VM use `vm status`. For example: 
+
+    owntime vm status FN-2187-0
+
+To **suspend** a particular VM use `vm suspend`. For example: 
+
+    owntime vm suspend FN-2187-0
+
+To **resume** a particular VM use `vm resume`. For example: 
+
+    owntime vm resume FN-2187-0
+    
+To **reboot** a particular VM use `vm reboot`. For example: 
+
+    owntime vm reboot FN-2187-0
+    
+To **delete** a particular VM use `vm delete`. For example: 
+
+    owntime vm delete FN-2187-0
+    
---
+++ owntime-manager.py
@@ -0,0 +1,261 @@
+#! /usr/bin/env python
+
+"""
+owntime-manager.py
+
+description:    Helper script for managing virtual machines (VMs) on Intersect's OwnTime (http://www.intersect.org.au/content/owntime-cloud-computing).
+author:         Jared Berghold
+copyright:      Copyright 2015, Intersect Australia Pty Ltd
+source:         https://github.com/IntersectAustralia/owntime-manager
+"""
+
+import sys, os, traceback
+from openstack import connection
+from openstack import exceptions
+
+def PrintUsage():
+    """
+    Print usage instructions
+    """
+    print "Usage:"
+    print ""
+    print "   Create a new VM: \t"
+    print "      " + os.path.basename(__file__) + " vm create <user_id> [-f \"flavour name\" -i \"image name\"]"
+    print ""
+    print "   List VMs for given user:"
+    print "      " + os.path.basename(__file__) + " user <user_id>"
+    print ""
+    print "   Manage VM for given user:"
+    print "      " + os.path.basename(__file__) + " vm [status|suspend|resume|reboot|delete] <vm_name>"
+
+def CreateConnection():
+    """
+    Create a connection to the Nectar Cloud using a clouds.yaml configuration file
+    clouds.yaml file can be stored in any of: 
+    * Current Directory
+    * ~/.config/openstack
+    * /etc/openstack
+    or a user-defined location by using "export OS_CLIENT_CONFIG_FILE=/path/to/my/config/my-clouds.yaml"
+    """
+    return connection.from_config(cloud_name='nectar')
+
+def GenerateKeypair(conn, userID):
+    """
+    Check if keypair exists for the userID. If not, create one.
+    """
+    kp = conn.compute.find_keypair(userID)
+    if kp is not None:
+        print "Using existing keypair: \'" + kp.name + "\'"
+    else:
+        dirname = os.path.dirname(os.path.realpath("__file__")) # get current directory
+        filename = os.path.join(dirname, userID)
+        filenamePrivate = filename + '.key'
+        filenamePublic = filenamePrivate + '.pub'
+        args = {'name': userID}
+        kp = conn.compute.create_keypair(**args)
+        with open(filenamePrivate, 'w') as f:
+            f.write("%s" % kp.private_key)
+        with open(filenamePublic, 'w') as f:
+            f.write("%s" % kp.public_key)
+        os.chmod(filenamePrivate, 0o600)
+        os.chmod(filenamePublic, 0o644)
+        print "Created new keypair, \'" + kp.name + "\' and saved it to " + dirname
+    return kp
+
+def CreateVM(conn, arguments):
+    """
+    Create a new VM for a given userID, with either default or user-defined flavour/image
+    """
+    flavourName = "m2.medium" # Default flavour
+    imageName = "NeCTAR CentOS 7 x86_64" # Default image
+    
+    iterArguments = iter(arguments)    
+    while True:
+      try:
+        arg = iterArguments.next()
+        if (arg == "-f"):
+            flavourName = iterArguments.next()
+        elif (arg == "-i"):
+            imageName = iterArguments.next()
+        else:
+            userID = arg
+      except StopIteration:
+            break
+    
+    print "Creating a new VM (flavour = \'" + flavourName + "\' image = \'" + imageName + "\') for \'" + userID + "\'..."
+    
+    # Choose a name for the VM - the name will use the userID as the prefix followed by a number
+    # Make sure the number used hasn't already been used before
+    vmNameSuffix = "-"
+    vmName = userID + vmNameSuffix + "0" # Set default machine name
+
+    servers = conn.compute.servers(details=True, name=r"" + userID + vmNameSuffix + "[0-9]+")
+
+    serverList = []
+    for server in servers:
+        serverList.append(int(server.name[len(userID) + len(vmNameSuffix):]))
+
+    if (len(serverList) > 0):
+        vmName = userID + vmNameSuffix + str(max(serverList) + 1)
+    
+    # Attempt to find the requested flavour and image    
+    flavour = conn.compute.find_flavor(flavourName, False)
+    image = conn.compute.find_image(imageName, False)
+        
+    # Use pre-existing key pair or create a new one
+    keypair = GenerateKeypair(conn, userID)
+    
+    # Create a new VM with the following properties
+    args = {
+        "name":vmName,
+        "flavor":flavour,
+        "security_groups":[{"name":"default"}],
+        "image":image,
+        "key_name": keypair.name,
+        "availability_zone":"intersect"
+    }
+
+    newVM = conn.compute.create_server(**args)
+
+    print "Successfully created new VM: \'" + vmName + "\'"
+
+    # Get IP address for the new VM
+    serverDetails = conn.compute.get_server(newVM.id)
+
+    while serverDetails.status != "ACTIVE":
+        serverDetails = conn.compute.get_server(newVM.id)
+
+    ipAddress = "0.0.0.0"
+    for key, value in serverDetails.addresses.items():
+        ipAddress = value[0].get('addr')
+        break
+
+    print "IP address of new VM is: " + ipAddress
+
+def ListVMs(conn, userID):
+    """
+    List all VMs associated with a given userID
+    """
+    print "Listing VMs for \'" + userID + "\':"
+    
+    servers = conn.compute.servers(details=True, name=r"" + userID + "-[0-9]+")
+
+    for server in servers:
+        print "Name: \'" + server.name + "\'\t",
+        ipAddress = "0.0.0.0"
+        for key, value in server.addresses.items():
+            ipAddress = value[0].get('addr')
+            break
+        print "IP: " + ipAddress
+
+def ManageVM(conn, vmName, action):
+    """
+    Get status for or suspend/resume/reboot/delete the VM
+    """
+    serverDetails = None
+    
+    # Fetch full server details up front as this will provide
+    # access to .status
+    servers = conn.compute.servers(details=True, name=vmName)
+    
+    for server in servers:
+        if (server.name == vmName):
+            serverDetails = server
+    
+    if (serverDetails is not None):        
+        # Fetch simple server object (i.e. not server details)
+        # as we call .action on this object
+        servers = conn.compute.servers(details=False, name=vmName)
+        for server in servers:
+            if (server.name == vmName):
+                if (action == "status"):
+                    print "\'" + vmName + "\' status is: " + serverDetails.status
+                elif (action == "suspend"):
+                    if (serverDetails.status == "ACTIVE"):
+                        print "Suspending \'" + vmName + "\'..." 
+                        try:
+                            server.action(session=conn.session,body={"suspend": None})
+                        except:
+                            pass
+                        while serverDetails.status != "SUSPENDED":
+                            serverDetails = conn.compute.get_server(serverDetails.id)
+                        print "Suspended \'" + vmName + "\'"
+                    else:
+                        print "\'" + vmName + "\' already suspended or inactive"
+                elif (action == "resume"):
+                    if (serverDetails.status == "SUSPENDED"):
+                        print "Resuming \'" + vmName + "\'..."
+                        try:
+                            server.action(session=conn.session,body={"resume": None})
+                        except:
+                            pass
+                        while serverDetails.status != "ACTIVE":
+                            serverDetails = conn.compute.get_server(serverDetails.id)
+                        print "Resumed \'" + vmName + "\'"
+                    else:
+                        print "\'" + vmName + "\' already active"
+                elif (action == "reboot"):
+                    print "Rebooting \'" + vmName + "\'..."
+                    try:
+                        server.action(session=conn.session,body={"reboot": {"type": "SOFT"}})
+                    except:
+                        pass
+                    while serverDetails.status != "ACTIVE":
+                        serverDetails = conn.compute.get_server(serverDetails.id)
+                    print "Rebooted \'" + vmName + "\'"
+                elif (action == "delete"):
+                    print "This action cannot be reversed. Are you sure you want to delete \'" + vmName + "\' (y/n)?"
+                    answer = raw_input().lower()
+                    if (answer == "y"):
+                        print "Deleting \'" + vmName + "\'..."
+                        conn.compute.delete_server(serverDetails.id)
+                        try:
+                            while True:
+                                conn.compute.get_server(serverDetails.id)
+                        except exceptions.ResourceNotFound as error:    
+                            print "Deleted \'" + vmName + "\'"
+                    else:
+                        print "Aborted delete"
+    else:
+        print "\'" + vmName + "\' could not be found" 
+
+def main(argv):
+    conn = CreateConnection()
+    if (conn is not None):
+        try:
+            if (len(argv) > 1):
+                if argv[1] == "vm":
+                    if argv[2] == "create":
+                        CreateVM(conn, argv[3:])
+                    elif argv[2] == "status":
+                        ManageVM(conn, argv[3], "status")
+                    elif argv[2] == "suspend":
+                        ManageVM(conn, argv[3], "suspend")
+                    elif argv[2] == "resume":
+                        ManageVM(conn, argv[3], "resume")
+                    elif argv[2] == "reboot":
+                        ManageVM(conn, argv[3], "reboot")
+                    elif argv[2] == "delete":
+                        ManageVM(conn, argv[3], "delete")
+                    else: # Ensures restricted vocab of the program's usage is adhered to
+                        PrintUsage()
+                elif argv[1] == "user":
+                    ListVMs(conn, argv[2])
+                else:
+                    PrintUsage()
+            else:
+                PrintUsage()
+        except Exception as ex:
+            exceptionString = str(ex)
+            print "Error:"
+            print "  Check Internet connection and availability of NecTAR Cloud service"
+            print "  Run \'" + os.path.basename(__file__) + "\' to see usage instructions"
+            print "  Exception Details: " + exceptionString
+            
+            # The following 3 lines are used for debugging only
+            #exc_type, exc_value, exc_traceback = sys.exc_info()
+            #print "*** PRINT TRACEBACK ***"
+            #traceback.print_tb(exc_traceback)
+
+if __name__ == "__main__":
+    main(sys.argv)
